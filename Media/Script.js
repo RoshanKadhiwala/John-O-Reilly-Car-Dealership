2b20164b4aa5ca05baa62dc4a591d5224cf41427
YBY-Hub <D24125569@mytudublin.ie>
2024-10-17 14:53:27 +0100

    Update and rename f to Script.js

---
+++ Media/Script.js
@@ -0,0 +1,13 @@
+function OpenRemoteControl(){
+
+    var anotherwindow=window.open("RemoteControl.html","RemoteCtrl","width=300,height=300")
+
+}
+function AlertOnMain(){
+    window.opener.alert("This Window is the opener")
+}
+
+function ColorSettings1(color1,color2,color3){
+// https://stackoverflow.com/questions/66241300/javascript-change-colour-of-element-on-click-with-if-statement#:~:text=You%20can%20use%20Element.,the%20color%20of%20your%20element.
+// check the above link later to set the specific colors brotha good luck
+}
